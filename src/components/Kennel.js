e3960e79672a5647bbaceb8e7b4f4aeded92eda3
samj0516 <samj0516@gmail.com>
2021-02-24 16:22:39 -0600

    finished Ch.1 book 4

---
+++ src/components/Kennel.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { AnimalCard } from "./animal/AnimalCard"
+import "./animal/Animal.css"
+import { EmployeeCard } from './employee/EmployeeCard'
+import { LocationCard } from './location/LocationCard'
+import { CustomerCard } from './customer/CustomerCard'
+export const Kennel = () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <small>Loving care when you're not there.</small>
+
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
+        <h2>Employees</h2>
+        <article className="animals">
+            <EmployeeCard />
+            <EmployeeCard />
+            <EmployeeCard />
+        </article>
+        <h2>Locations</h2>
+        <article className="animals">
+            <LocationCard />
+            <LocationCard />
+        </article>
+        <h2>Customers</h2>
+        <article className="animals">
+            <CustomerCard />
+            <CustomerCard />
+            <CustomerCard />
+            <CustomerCard />
+        </article>
+    </>
+)
---
+++ src/components/location/LocationCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="animal">
+        <h3 className="animal__name">Nashville Kennels North</h3>
+        <div className="animal__breed">200 Main Street</div>
+    </section>
+)
