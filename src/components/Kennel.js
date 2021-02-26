51358a1a09af32ac0f59b6e90055d9d04df15eff
samj0516 <samj0516@gmail.com>
2021-02-25 23:43:35 -0600

    completed ch.4 book4 routing with react

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -4,39 +4,73 @@ import "./animal/Animal.css"
 import { EmployeeCard } from './employee/EmployeeCard'
 import { LocationCard } from './location/LocationCard'
 import { CustomerCard } from './customer/CustomerCard'
+import { PropsAndState } from './PropsAndState'
+import { NavBar } from './nav/NavBar'
+import { ApplicationViews } from './ApplicationViews'
+
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
+         <NavBar />
+        <ApplicationViews />
         <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
+    <article className="animals">
+        <AnimalCard />
+        <AnimalCard />
+        <AnimalCard />
+    </article>
         <h2>Employees</h2>
-        <article className="animals">
-            <EmployeeCard />
-            <EmployeeCard />
-            <EmployeeCard />
-        </article>
+    <article className="animals">
+    <EmployeeCard />
+    <EmployeeCard />
+    <EmployeeCard />
+</article>
         <h2>Locations</h2>
-        <article className="animals">
-            <LocationCard />
-            <LocationCard />
-        </article>
-        <h2>Customers</h2>
-        <article className="animals">
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-        </article>
+    <article className="animals">
+    <LocationCard />
+    <LocationCard />
+</article>
+    <h2>Customers</h2>
+    <article className="animals">
+        <CustomerCard />
+        <CustomerCard />
+        <CustomerCard />
+        <CustomerCard />
+    </article> 
+     
     </>
 )
+
+
+
+// { <h2>Nashville Kennels</h2>
+// <small>Loving care when you're not there.</small>
+
+// <address>
+//     <div>Visit Us at the Nashville North Location</div>
+//     <div>500 Puppy Way</div>
+// </address>
+// <PropsAndState yourName={"Samantha"} />
+// <h2>Animals</h2>
+// <article className="animals">
+//     <AnimalCard />
+//     <AnimalCard />
+//     <AnimalCard />
+// </article>
+// <h2>Employees</h2>
+// <article className="animals">
+//     <EmployeeCard />
+//     <EmployeeCard />
+//     <EmployeeCard />
+// </article>
+// <h2>Locations</h2>
+// <article className="animals">
+//     <LocationCard />
+//     <LocationCard />
+// </article>
+// <h2>Customers</h2>
+// <article className="animals">
+//     <CustomerCard />
+//     <CustomerCard />
+//     <CustomerCard />
+//     <CustomerCard />
+// </article> }
