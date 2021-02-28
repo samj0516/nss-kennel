34dbb3667dbbc379cff132271ec5010086bff1b4
samj0516 <samj0516@gmail.com>
2021-02-28 17:20:42 -0600

    completed ch. 6 Listing Animals, animal cards render with data from api

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { AnimalCard } from "./animal/AnimalCard"
+// import { AnimalCard } from "./animal/AnimalCard"
 import "./animal/Animal.css"
 import { EmployeeCard } from './employee/EmployeeCard'
 import { LocationCard } from './location/LocationCard'
 import { CustomerCard } from './customer/CustomerCard'
-import { PropsAndState } from './PropsAndState'
+// import { PropsAndState } from './PropsAndState'
 import { NavBar } from './nav/NavBar'
 import { ApplicationViews } from './ApplicationViews'
 
@@ -12,30 +12,7 @@ export const Kennel = () => (
     <>
          <NavBar />
         <ApplicationViews />
-        <h2>Animals</h2>
-    <article className="animals">
-        <AnimalCard />
-        <AnimalCard />
-        <AnimalCard />
-    </article>
-        <h2>Employees</h2>
-    <article className="animals">
-    <EmployeeCard />
-    <EmployeeCard />
-    <EmployeeCard />
-</article>
-        <h2>Locations</h2>
-    <article className="animals">
-    <LocationCard />
-    <LocationCard />
-</article>
-    <h2>Customers</h2>
-    <article className="animals">
-        <CustomerCard />
-        <CustomerCard />
-        <CustomerCard />
-        <CustomerCard />
-    </article> 
+     
      
     </>
 )
