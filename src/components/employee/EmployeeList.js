34ad0941a65a62f3dab5a1188ffbbb56c5462233
samj0516 <samj0516@gmail.com>
2021-03-01 16:29:33 -0600

    finished ch9 hire employee practice. Button renders form that adds a new employee to db and rerenders employeeList with new employee

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,28 +1,36 @@
 import React, { useContext, useEffect } from "react"
+import { useHistory } from 'react-router-dom'
 import { EmployeeContext } from "./EmployeeProvider"
 import { EmployeeCard } from "./EmployeeCard"
+import { LocationContext } from '../location/LocationProvider'
 import "./Employee.css"
 
 export const EmployeeList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { employees, getEmployees } = useContext(EmployeeContext)
-
+  const { locations, getLocations } = useContext(LocationContext)
+  
   //useEffect - reach out to the world for something
   useEffect(() => {
-    // console.log("CustomerList: useEffect - getCustomers")
-    getEmployees()
+    getLocations()
+    .then(getEmployees)
 
   }, [])
 
-  
+  const history = useHistory()
   return (
-    <div className="employees">
-      {/* {console.log("AnimalList: Render", animals)} */}
+    <>
+      	      <button onClick={() => {history.push("/employees/create")}}>
+            Hire Employee
+          </button>
+          <div className="employees">
       {
         employees.map(employee => {
-          return <EmployeeCard key={employee.id} employee={employee} />
+          const clinic = locations.find(l => l.id === employee.locationId)
+          return <EmployeeCard key={employee.id} employee={employee} location={clinic} />
         })
       }
     </div>
+    </>
   )
 }
