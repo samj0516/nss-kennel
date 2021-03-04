da2beec648128b6dd1dadd406b7b6ae6f681fcd4
samj0516 <samj0516@gmail.com>
2021-03-04 10:39:38 -0600

    finished ch12 releasing animal

--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -28,6 +28,13 @@ export const AnimalProvider = (props) => {
             .then(res => res.json())
     }
 
+    const releaseAnimal = animalId => {
+        return fetch(`http://localhost:8088/animals/${animalId}`, {
+            method: "DELETE"
+        })
+            .then(getAnimals)
+    }
+
     /*
         You return a context provider which has the
         `animals` state, `getAnimals` function,
@@ -36,7 +43,7 @@ export const AnimalProvider = (props) => {
     */
     return (
         <AnimalContext.Provider value={{
-            animals, getAnimals, addAnimal, getAnimalById
+            animals, getAnimals, addAnimal, getAnimalById, releaseAnimal
         }}>
             {props.children}
         </AnimalContext.Provider>
