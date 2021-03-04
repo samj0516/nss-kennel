bf22f4958d9cced60144db0283efa18dbb790d12
samj0516 <samj0516@gmail.com>
2021-03-04 00:06:14 -0600

    finished ch.11 location details

--- src/components/employee/EmployeeProvider.js
+++ src/components/employee/EmployeeProvider.js
@@ -23,6 +23,10 @@ export const EmployeeProvider = (props) => {
         })
         .then(getEmployees)
     }
+    const getEmployeeById = (id) => {
+        return fetch(`http://localhost:8088/employees/${id}?_expand=location`)
+            .then(res => res.json())
+    }
 
     /*
         You return a context provider which has the
@@ -32,7 +36,7 @@ export const EmployeeProvider = (props) => {
     */
     return (
         <EmployeeContext.Provider value={{
-            employees, getEmployees, addEmployee
+            employees, getEmployees, addEmployee, getEmployeeById
         }}>
             {props.children}
         </EmployeeContext.Provider>
--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 import "./Location.css"
 
 export const LocationCard = ({ location }) => (
     <section className="animal">
-        <h3 className="animal__name">{location.name}</h3>
-        <div className="animal__breed">{location.address}</div>
+        <h3 className="animal__name">  
+            <Link to={`/locations/detail/${location.id}`}>
+                { location.name }
+            </Link></h3>
+        {/* <div className="animal__breed">{location.address}</div> */}
     </section>
 )
 
